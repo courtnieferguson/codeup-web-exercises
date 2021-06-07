065b4510754001d45be4f48f7e917ccbed5e0471
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-06-07 11:34:37 -0500

    array exercises

---
+++ js/array_classwork.js
@@ -0,0 +1,15 @@
+'use strict';
+
+alert("Array Classwork!");
+
+let test = [];
+
+/*
+array is a memory that stores more than one thing
+*/
+    test = [100, 100, 90, 95, 90, 100]
+
+    var dogs = [];
+    dogs = ["Tommy", "Boots", "Bellamy"];
+
+console.log(dogs);
--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -15,10 +15,30 @@ for (let index = 0; index < 50; index++) {
         console.log("Yikes! Skipping number: " + index);
         continue
     }
-    if (index %2 !=0) {
+    if (index % 2 != 0) {
         console.log("Here is an odd number: " + index);
-        if (index %2 ==0) {
+        if (index % 2 == 0) {
             continue;
         }
-        console.log("Here is an odd number: " + index);
+        console.log("Here is an odd number: " + index)
     }
+}
+
+while(true) {
+    let odd_Number = prompt("Enter an odd number between 1 and 50");
+    if ((odd_Number != 1) && (odd_Number<=50)) {
+        if (odd_Number % 2 != 0) {
+            break;
+            }
+      }
+}
+
+console.log("Number to skip is: " + odd_Number);
+
+for (let number = 1; number < 50; number = number + 2) {
+    if (odd_Number == number) {
+        console.log("Yikes! Skipping number: " + odd_Number);
+        continue;
+    }
+    console.log("Here is an odd number: " + number);
+}
