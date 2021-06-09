97a44bc10a54114cca2e2a7e82d18268d0fc9167
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-06-09 10:58:30 -0500

    objects

---
+++ js/math.classwork.js
@@ -0,0 +1,22 @@
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) * min);
+}
+
+console.log(getRandomInt(1,2)); // coin toss
+console.log(getRandomInt(1,52)); // deck of cards
+console.log(getRandomInt)(1,6));// dice
+
+console.log(Math.round(23.5234)); //round
+
+console.log(Math.floor)(23.52340));
+
+console.log(Math.ceil)(23.52340));
+
+console.log(Math.pow(3,3));
+
+console.log(Math.sqrt(25));
+
+console.log(Math.E);
+
+console.log(Math.PI);
