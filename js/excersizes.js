6471211e5262a3518cfa1d80e3c833e9e2ade273
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-06-23 08:40:00 -0500

    bootstrap pizza

---
+++ js/excersizes.js
@@ -0,0 +1,28 @@
+/*function addUp(x) {
+    var sum = 0;
+    if (isNumeric(x)) {
+        for ()
+    }
+}*/
+function isNumeric(x) {
+    (!isNaN)parseFloat(x))
+}
+
+function calcAge (age) {
+    if (isNumeric(age)) {
+        return (age * 365);
+    } else {
+        return NaN
+    }
+}
+
+function sumPolygon(n) {
+    if (isNumeric(n)) && n>2) {
+        return (n-2) * 100;
+    }
+    return "Value is not the bunber of sides of a polygon"
+}
+
+function sumAll (array) {
+    var =
+}
